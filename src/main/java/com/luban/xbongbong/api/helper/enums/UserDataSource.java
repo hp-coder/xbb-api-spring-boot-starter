96bc62e637b357a1537d6117145b975fe3e608de
胡鹏 <hup@lubanjianye.com>
2023-01-05 16:00:59 +0800

    增加一个用户来源的枚举

---
+++ src/main/java/com/luban/xbongbong/api/helper/enums/UserDataSource.java
@@ -0,0 +1,21 @@
+package com.luban.xbongbong.api.helper.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+/**
+ * @author HP 2023/1/5
+ */
+@Getter
+@AllArgsConstructor
+public enum UserDataSource {
+
+    XBB("销帮帮自建"),
+    LEBIAO_CMS("乐标后台"),
+    DIAO_XIAO("电销平台"),
+    APP("app后台"),
+    ;
+
+    private String name;
+}
