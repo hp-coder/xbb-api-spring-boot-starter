30e0d94a18435486d7c53125e5ba03e4ebb77fa1
胡鹏 <hup@lubanjianye.com>
2023-01-10 16:36:38 +0800

    自动配置类调整

--- src/main/java/com/luban/xbongbong/api/XbbApiAutoConfiguration.java
+++ src/main/java/com/luban/xbongbong/api/XbbApiAutoConfiguration.java
@@ -4,6 +4,7 @@ import cn.hutool.extra.spring.EnableSpringUtil;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -13,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * @author HP 2022/12/27
  */
+@Configuration
 @EnableSpringUtil
 @EnableConfigurationProperties
 public class XbbApiAutoConfiguration {
